24e0232fc269aa9b2b569fe847aedcfc83bad6b9
sir-meee <sammymbevi@gmail.com>
2021-03-29 15:15:48 +0300

    create models

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+# from django.contrib,auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# User = get_user_model()
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    # phoned = models.BooleanField(default=False)
+
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField()
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
