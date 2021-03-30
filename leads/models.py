e89a6971fb861bbe63c13482b73c642cbbd9c5d7
sir-meee <sammymbevi@gmail.com>
2021-03-30 17:42:16 +0300

    URL Namespaces, Lead List and Detail Views, Forms and Create View

--- leads/models.py
+++ leads/models.py
@@ -15,7 +15,12 @@ class Lead(models.Model):
 
     # profile_picture = models.ImageField(blank=True, null=True)
     # special_files = models.FileField()
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Lead, Agent
+from .forms import LeadForm
 
-# Create your views here.
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/lead_list.html", context)
+
+def lead_detail(request, pk):
+    # print(pk)
+    lead = Lead.objects.get(pk=pk)
+    # print(lead)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+def lead_create(request):
+    form = LeadForm()
+    # print(request.POST)
+    if request.method == "POST":
+        # print('Receiving a post request')
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            # print("The form is valid")
+            # print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name = first_name,
+                last_name = last_name,
+                age = age,
+                agent = agent
+            )
+            # print("The lead has been created")
+            return redirect("/leads")
+    context = {
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context)
