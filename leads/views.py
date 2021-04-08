cb0942c53a2ce7187c05a789369a0ade9d56cbec
sir-meee <sammymbevi@gmail.com>
2021-04-08 18:57:00 +0300

    Unassigned Leads

--- leads/views.py
+++ leads/views.py
@@ -34,14 +34,27 @@ class LeadListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         user = self.request.user
-        # initial queryset of opportunities for the entire organisation
+        # initial queryset of leads for the entire organisation
         if user.is_organisor:
-            queryset = Lead.objects.filter(organisation=user.userprofile)
+            queryset = Lead.objects.filter(organisation=user.userprofile, agent__isnull=False)
         else:
-            queryset = Lead.objects.filter(organisation=user.agent.organisation)
+            queryset = Lead.objects.filter(organisation=user.agent.organisation, agent__isnull=False)
             # filter for the agent that is logged in
             queryset = queryset.filter(agent__user=user)
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(LeadListView, self).get_context_data(**kwargs)
+        user = self.request.user
+        if user.is_organisor:
+            queryset = Lead.objects.filter(
+                organisation=user.userprofile, 
+                agent__isnull=True
+            )
+            context.update({
+                "unassigned_leads": queryset
+            })
+        return context
 
 # def lead_detail(request, pk):
 #     # print(pk)
@@ -58,7 +71,7 @@ class LeadDetailView(LoginRequiredMixin, generic.DetailView):
 
     def get_queryset(self):
         user = self.request.user
-        # initial queryset of opportunities for the entire organisation
+        # initial queryset of leads for the entire organisation
         if user.is_organisor:
             queryset = Lead.objects.filter(organisation=user.userprofile)
         else:
@@ -165,7 +178,7 @@ class LeadUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
 
     def get_queryset(self):
         user = self.request.user
-        # initial queryset of opportunities for the entire organisation
+        # initial queryset of leads for the entire organisation
         return Lead.objects.filter(organisation=user.userprofile)
     
     def get_success_url(self):
@@ -181,7 +194,7 @@ class LeadDeleteView(OrganisorAndLoginRequiredMixin, generic.DeleteView):
     
     def get_queryset(self):
         user = self.request.user
-        # initial queryset of opportunities for the entire organisation
+        # initial queryset of leads for the entire organisation
         return Lead.objects.filter(organisation=user.userprofile)
     
     def get_success_url(self):
