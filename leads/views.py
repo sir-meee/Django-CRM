57d1173ae3e1f1bb7bc40a4cd5a851cd8924b7ab
sir-meee <sammymbevi@gmail.com>
2021-04-10 13:57:44 +0300

    message styles

--- leads/views.py
+++ leads/views.py
@@ -240,6 +240,11 @@ class LeadUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
     
     def get_success_url(self):
         return reverse("leads:lead-list")
+    
+    def form_valid(self, form):
+        form.save()
+        messages.info(self.request, "You have successfully updated this lead")
+        return super(LeadUpdateView, self).form_valid(form)
 
 # def lead_delete(request, pk):
 #     lead = Lead.objects.get(pk=pk)
