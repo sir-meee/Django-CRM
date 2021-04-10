9f387ccc0b87abe5643c89f55f69d5121178845a
sir-meee <sammymbevi@gmail.com>
2021-04-10 11:16:25 +0300

    profile picture field

--- leads/forms.py
+++ leads/forms.py
@@ -24,6 +24,7 @@ class LeadModelForm(forms.ModelForm):
             'description',
             'phone_number',
             'email',
+            'profile_picture'
         )
 
     def clean_first_name(self):
