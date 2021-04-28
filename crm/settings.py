0627ad55236c79ee7559f6a80483d869ab7fa540
sir-meee <sammymbevi@gmail.com>
2021-04-28 10:44:48 +0300

    added database connection for heroku

--- crm/settings.py
+++ crm/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 import dj_database_url
-db_from_env = dj_database_url(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 
